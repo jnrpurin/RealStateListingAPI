c4863761f7ebd9fa5493be288d6ca3858b2f4d9d
Ademir <jnrpurin@gmail.com>
2026-01-06 15:13:43 -0300

    Feat - Create unit tests and simple adjusts on ignore files.

---
+++ tests/RealEstateListingApi.Tests/Factory/UnitOfWorkMockFactory.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using RealEstateListingApi.UnitOfWork;
+using RealEstateListingApi.Repo;
+using RealEstateListingApi.Models;
+
+namespace RealEstateListingApi.Tests.Factory
+{
+    public static class UnitOfWorkMockFactory
+    {
+        public static Mock<IUnitOfWork> Create(Listing listing)
+        {
+            var repoMock = new Mock<IListingRepo>();
+            repoMock.Setup(r => r.GetById(It.IsAny<Guid>()))
+                    .Returns(listing);
+
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock.Setup(u => u.Listings)
+                   .Returns(repoMock.Object);
+
+            return uowMock;
+        }
+    }
+}
